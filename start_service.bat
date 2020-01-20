f323f3821c9fa9efee436cc17c3eb72b7839093c
Garg <gargsumeet1988@gmail.com>
2020-01-20 13:45:48 -0500

    first commit

---
+++ start_service.bat
@@ -0,0 +1,7 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ---------------------------------------------------------------------------
+rem Start script for the Jira Service
+rem
+
+net start JiraSoftware221219134221
---
+++ stop_service.bat
@@ -0,0 +1,7 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ---------------------------------------------------------------------------
+rem Stop script for the Jira Service
+rem
+
+net stop JiraSoftware221219134221
